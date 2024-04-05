c7be62bc47851e3b07f33a536cd6af8c9dbbb935
horitomoyuki <horitomoyuki930131@gmail.com>
2024-04-05 16:02:48 +0900

    インデントが変・・

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Hello Golang\nLet's Study Golang\n")
-	sl := []int{1, 2, 3}
-	if len(sl) > 2 {
-		fmt.Println("unreachable code")
-	}
+ fmt.Printf("Hello Golang\nLet's Study Golang\n")
+ sl := []int{1, 2, 3}
+ if len(sl) > 2 {
+ fmt.Println("unreachable code")
+}
 }
