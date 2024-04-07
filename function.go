5c7028ee631a1162c04e8e3783dc3a4f646fc0fe
horitomoyuki <horitomoyuki930131@gmail.com>
2024-04-07 15:26:42 +0900

    fileChecker

--- function.go
+++ function.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"strings"
+	"errors"
+  "fmt"
+	"os"
+  "strings"
 )
 
 func funcDefer() {
@@ -10,16 +12,30 @@ func funcDefer() {
 	defer fmt.Println("main func semi-finish")
 	fmt.Println("hello world")
 }
-func trimExtension(files ...string) []string { // 引数で3ドットString 可変長で受け取るテキスト一覧を関数内でテキストのスライスとして扱える
+func trimExtension(files ...string) []string { // 可変長で受け取るテキスト一覧を関数内でテキストのスライスとして扱える
 	out := make([]string, 0, len(files)) // 引数で受け取ったファイル名から拡張子を取り除くスライスをmake関数で定義
 	for _, f := range files { // インデックスは使用しないので_で受け取る
 		out = append(out, strings.TrimSuffix(f, ".csv"))
 	}
 	return out
 }
+func fileChecker(name string) (string, error) { // チェックするファイル名をString型 返り値にString型とエラー型
+	f, err := os.Open(name) // 標準パッケージのOSOPENを使う
+	if err != nil { // エラーの場合
+		return "", errors.New("file not found") // エラー出力をして関数を抜ける
+	}
+	defer f.Close() // 開いておいたリソースを閉じる
+	return name, nil // 成功している場合はリターンで受け取ったファイル名とエラーの値は無いのでnilを返す
+}
 
 func main() {
 	funcDefer()
 	files := []string{"file1.csv", "file2.csv", "file3.csv"} // String型のスライスをつくり3つのファイル名で初期化
 	fmt.Println(trimExtension(files...)) // filesのStringスライスを可変長引数の形に変換
+	name, err := fileChecker("file.txt") // 返り値は2つなのでnameとerrという名前で受け取れるように
+	if err != nil { // 呼び出した側でもエラーチェックをする
+		fmt.Println(err)
+		return // returnでmain関数を抜ける
+	}
+	fmt.Println(name)
 }
